e468ded42490e8d650f0b58a745dbdf8f6a71a99
Mark Nguyen <mark.nguyen.9614@gmail.com>
2019-10-06 01:43:51 +1000

    Added some basic commenting

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Front-end
+## Setting up environment
+pip3 install virtualenv
+virutalenv -p python3 venv --always-copy
+source venv/bin/activate
+
+## Dependencies to install in virtual environment
+pip install flask
+pip install flask-bootstrap
+pip install boto3
+pip install awscli
+pip install arrow
+
+## Configuring AWS
+aws configure
+
+## If .env doesnt work
+export FLASK_APP=app.py
+export FLASK_ENV=development
+
+## To Run the server
+flask run
+flask run --host=0.0.0.0
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 import boto3
 from config import S3_BUCKET,S3_KEY,S3_SECRET_ACCESS_KEY
 
+#setup boto 3 resources,
 s3_resource = boto3.resource(
     "s3",
     aws_access_key_id=S3_KEY,
@@ -19,9 +20,12 @@ def index():
 @app.route('/files')
 def files():
     s3_resource = boto3.resource('s3')
+    # makes a bucket variable that grabs the bucket 
+    # information from the specified bucket
     my_bucket = s3_resource.Bucket(S3_BUCKET)
-    summaries = my_bucket.objects.all()
 
+    # grabs all the summaries (contents) of the s3 bucket
+    summaries = my_bucket.objects.all()
     return render_template('files.html', my_bucket=my_bucket, files=summaries)
 
 if __name__ == '__main__':
