b1a58f7462ba4b8d53025766d2e4766f729d1a92
Mark Nguyen <mark.nguyen.9614@gmail.com>
2019-10-06 03:14:29 +1000

    Added two pages , one for upload and one for download

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 import boto3
-from config import S3_BUCKET,S3_KEY,S3_SECRET_ACCESS_KEY
+from config import S3_BUCKET,S3_BUCKET2,S3_KEY,S3_SECRET_ACCESS_KEY
+from filters import timeformat, file_type
 
 #setup boto 3 resources,
 s3_resource = boto3.resource(
@@ -13,11 +14,14 @@ s3_resource = boto3.resource(
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+app.jinja_env.filters['timeformat'] = timeformat
+app.jinja_env.filters['file_type'] = file_type
 
-@app.route('/files')
+#@app.route('/')
+#def index():
+#    return render_template("index.html")
+
+@app.route('/')
 def files():
     s3_resource = boto3.resource('s3')
     # makes a bucket variable that grabs the bucket 
@@ -27,6 +31,17 @@ def files():
     # grabs all the summaries (contents) of the s3 bucket
     summaries = my_bucket.objects.all()
     return render_template('files.html', my_bucket=my_bucket, files=summaries)
+
+@app.route('/download')
+def download():
+    s3_resource = boto3.resource('s3')
+    # makes a bucket variable that grabs the bucket 
+    # information from the specified bucket
+    my_bucket = s3_resource.Bucket(S3_BUCKET2)
+
+    # grabs all the summaries (contents) of the s3 bucket
+    summaries = my_bucket.objects.all()
+    return render_template('download.html', my_bucket=my_bucket, files=summaries)
 
 if __name__ == '__main__':
     app.run()
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 S3_BUCKET = os.environ.get("S3_BUCKET")
+S3_BUCKET2 = os.environ.get("S3_BUCKET2")
 S3_KEY = os.environ.get("S3_KEY")
 S3_SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_ACCESS_KEY")
